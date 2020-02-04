e5f0fc045f8dc7922ff6467ffa501b81ef351a85
Daniel Whiteman <daniel.whiteman1@gmail.com>
2020-02-04 08:56:01 -0600

    Added project files

---
+++ app.js
@@ -0,0 +1,21 @@
+function beautifulDays(i, j, k) {
+    let count = 0;
+    for(let x=i; x<=j; x++) {
+      if(isBeautifulDay(x, k)) {
+        count++;
+      }
+    }
+    return count;
+  };
+  
+  function isBeautifulDay(x, k) {
+    return differenceOfReverse(x) % k === 0;
+  }
+  
+  function differenceOfReverse (x) {
+    let reversedX = parseInt(x.toString().split('').reverse().join(''));
+    return Math.abs(x-reversedX);
+  }
+  
+  console.log(beautifulDays(4));
+  console.log(isBeautifulDay(4));
